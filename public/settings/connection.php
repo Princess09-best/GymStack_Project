285ddf0375a02dcb817b0db63f15c440e668f837
Princess09-best <105990515+Princess09-best@users.noreply.github.com>
2024-03-17 13:17:55 +0000

    database connection and error messages

--- public/settings/connection.php
+++ public/settings/connection.php
@@ -5,12 +5,12 @@ $username = "root";
 $password = "@StrateGicPlan20178##";
 $database = "gym";
 
-// Use mysqli connection method
+// Use mysqli connection method to connect to data base
 $mysqli = new mysqli($servername, $username, $password, $database);
 
-// Check if the connection worked
+// Check if the connection worked. if not, log the error and display a message to the user
 if ($mysqli->connect_error) {
     error_log("Connection failed: " . $mysqli->connect_error);
-    die("Sorry, something went wrong. Please try again later.");
+    die("Sorry, Connection could not be made at this time. Please try again later or check your code.");
 }
 ?>
